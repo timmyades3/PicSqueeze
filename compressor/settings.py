7deae1b080d8cd269f391d05a90b6b78923ddfe4
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-09-10 22:46:52 +0100

    updated

--- compressor/settings.py
+++ compressor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('COMPRESSOR_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost','picsqueeze.onrender.com']
 
 
 # Application definition
