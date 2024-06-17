7b66413a5d9bb6d81e15378f9bf37310773b6eb4
Adesina Abdulrahman <103801534+timmyades3@users.noreply.github.com>
2024-06-17 08:57:37 +0100

    Update views.py

--- compressor1/views.py
+++ compressor1/views.py
@@ -126,9 +126,8 @@ def upload_to_s3(image_bytes, file_name, save_path):
     session = boto3.Session(
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name=settings.AWS_S3_REGION_NAME
     )
-    s3 = session.client('s3')
+    s3 = session.client('s3',region_name=settings.AWS_S3_REGION_NAME)
 
     s3_key = f'{save_path}/{file_name}'
     s3.upload_fileobj(image_bytes, settings.AWS_STORAGE_BUCKET_NAME, s3_key)
