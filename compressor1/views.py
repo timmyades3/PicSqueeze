d7fc79ddd9e13471c075ed0a75f7829254b3162c
Adesina Abdulrahman <103801534+timmyades3@users.noreply.github.com>
2024-06-17 08:35:54 +0100

    Update views.py

--- compressor1/views.py
+++ compressor1/views.py
@@ -125,7 +125,8 @@ class Compress(View):
 def upload_to_s3(image_bytes, file_name, save_path):
     session = boto3.Session(
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION_NAME
     )
     s3 = session.client('s3')
 
