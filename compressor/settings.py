a898ebd317e8c0251c8964b67360068644c472fc
Adesina Abdulrahman <103801534+timmyades3@users.noreply.github.com>
2024-06-17 12:20:59 +0100

    Update settings.py

--- compressor/settings.py
+++ compressor/settings.py
@@ -93,15 +93,15 @@ WSGI_APPLICATION = 'compressor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#if DEBUG == True:
-#    DATABASES = {
-#        'default': {
-#           'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#    }
-#else:
-DATABASES = {
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+   DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('PGDATABASE'),
